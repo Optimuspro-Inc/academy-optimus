7be55567d1dc189b3eca9f773217d5338740b4a7
greatAdams01 <kingifean@gmail.com>
2022-07-30 02:11:59 +0100

    Added desktop nav

--- src/components/nav/Desktop.vue
+++ src/components/nav/Desktop.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="bg-white pt-8">
     <nav>
-      <div class="container">
-        <div class="logo flex justify-between w-[520px]">
+      <div class="container flex justify-between">
+        <div class="flex justify-between w-[520px]">
           <img class="w-[140px]" src="/images/logo.png" alt="">
           <div>
             <ul class="flex p-2">
-              <li class="inter pl-8" v-for="link in links" :key="link.title">
+              <li class="inter font-semibold text-sm pl-8" v-for="link in links" :key="link.title">
                 <RouterLink to="/">{{ link.title }}</RouterLink>
               </li>
             </ul>
           </div>
+        </div>
+        <div class="w-[308px] flex justify-between">
+          <button class="text-[#162044] border-[#162044] border-[1px] border-solid w-[140px] h-[45px] rounded-[53px]">Login</button>
+          <button class="text-white border-[#162044] border-[1px] border-solid w-[140px] h-[45px] rounded-[53px] bg-[#162044]">Sign Up</button>
         </div>
       </div>
     </nav>
