f4dd9f8f7a92328478a04914eae3acb116c3bbb5
OpeOginni <brightoginni123@gmail.com>
2022-11-18 16:40:02 +0100

    Another Try

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView, // This is the right component declaration
+      component: HomeView,
     },
     {
       path: "/about",
@@ -17,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"), // DO not use this type of componet declaration
+      component: () => import("../views/AboutView.vue"),
     },
     {
       path: "/cohorts",
